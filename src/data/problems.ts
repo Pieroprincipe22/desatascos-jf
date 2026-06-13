c3f4ae7d085cd6410b6ce24cbb684a2496e87022
Pieroprincipe22 <157012089+Pieroprincipe22@users.noreply.github.com>
2026-06-13 21:14:03 +0200

    Update frequent problems for phase 2

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,10 +1,12 @@
-﻿export const problems = [
+export const problems = [
   "WC atascado",
   "Fregadero que no traga",
   "Ducha o bañera atascada",
   "Tuberías con malos olores",
-  "Bajantes atascadas",
-  "Atascos en comunidades",
+  "Agua que baja lentamente",
+  "Atascos que vuelven a aparecer",
+  "Tuberías con suciedad acumulada",
   "Fugas de agua",
   "Averías de fontanería",
+  "Urgencias que no pueden esperar",
 ];
